e2519d7abfc04f04b644767d2162feb20906acaa
David Moreno <dmorgam94@gmail.com>
2019-05-11 12:39:29 +0200

    Update Microk8s push image

--- build-moodle-docker.sh
+++ build-moodle-docker.sh
@@ -3,5 +3,7 @@
 #--- Build local image and push to mickrok8s local ------
 
 cd moodle-kube/
-docker build -t localhost:32000/moodle_image:local .
+docker build -t moodle_image:local .
+docker save moodle_image > /tmp/moodle_image.tar
+microk8s.ctr -n k8s.io image import /tmp/moodle_image.tar
 cd ..
