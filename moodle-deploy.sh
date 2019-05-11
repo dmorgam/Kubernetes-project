63a2e88a62cd27fcbc6224d36d748274b1640d60
David Moreno <dmorgam94@gmail.com>
2019-05-11 10:45:20 +0200

    Add a custom Moodle Image

--- moodle-deploy.sh
+++ moodle-deploy.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-#---- Deploy Mariadb ---------
+#---- Build Docker Image for Moodle ----
+
+cd moodle-kube/
+docker build -t moodle_image:local .
+cd ..
+
+#---- Deploy Mariadb -------------------
 
 kubectl create -f moodle-kube/moodle-db.yml
 
 
-#---- Deploy Moodle ----------
+#---- Deploy Moodle --------------------
 
 kubectl create -f moodle-kube/moodle-deploy.yml
 
-#---- Node Port --------------
+#---- Node Port ------------------------
 
 kubectl expose service moodle --type=NodePort --name=moodle-nodeport
